f9a9114998f1975c5595756a6120c91a4176ddb7
florian.llimos <florian.llimos@digitanie.org>
2024-11-22 14:08:14 +0100

    feat: add script for change player

--- script.js
+++ script.js
@@ -149,3 +149,49 @@ function randomDice () {
         }
     } 
 
+let diceTruc = document.getElementById('roll');
+diceTruc.addEventListener("click", randomDice, null);
+
+// CHANGEMENT DE JOUEUR
+
+function changePlayer (notFalse) {
+    let playerOne = document.getElementById('p-1');
+    let playTwo = document.getElementById("p-2")
+    let winner = document.getElementById('winner').value;
+    diceClear();
+    if (playerOne.className === "play"){
+        if (totalP1 < 100) {
+            playerOne.classList.remove("play")
+            playTwo.classList.add("play");
+            if (notFalse) {
+                totalP1 = totalP1 + player1;
+                document.getElementById('player1').innerHTML = totalP1 ;
+            } if (totalP1 >= 100) {
+                document.getElementById('winner').innerHTML = 'Le joueur 1 remporte la partie !';
+                change.removeEventListener("click", changePlayer, null);
+                diceTruc.removeEventListener("click", randomDice, null);
+            }
+            player1 = 0;
+            document.getElementById('currentP1').innerHTML = player1;
+            } else {
+                console.log('Waiting a winner');
+        }
+    } else {
+        if (totalP2 < 100) {
+            playTwo.classList.remove("play")
+            playerOne.classList.add("play");
+            if (notFalse) {
+                totalP2 = totalP2 + player2;
+                document.getElementById('player2').innerHTML = totalP2;
+            } if (totalP2 >= 100) {
+                document.getElementById('winner').innerHTML = 'Le joueur 2 remporte la partie !';
+                change.removeEventListener("click", changePlayer, null);
+                diceTruc.removeEventListener("click", randomDice, null);
+            }
+            player2 = 0;
+            document.getElementById('currentP2').innerHTML = player2;
+        } else {
+            console.log('Waiting a winner');
+        }
+    }
+}  
